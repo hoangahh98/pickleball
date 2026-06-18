f6b6b3577d71f2d69fee45b6c61fa5e637ac78d2
Nguyen Khac Hoang Anh <hoangahh98@gmail.com>
2026-06-18 17:28:32 +0700

    abc

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 DB_CONFIG = {
     "dbname": "postgres",
-    "user": "postgres",
+    "user": "postgres.tmtrbnobadglqgcrgirr",
     "password": "Baobeo@2023",
-    "host": "db.tmtrbnobadglqgcrgirr.supabase.co",
+    "host": "aws-1-ap-northeast-1.pooler.supabase.com",
     "port": "5432"
 }
